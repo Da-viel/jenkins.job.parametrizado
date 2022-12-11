b7a47f6bc86d1abce11a0ce1a59fbb439829c3fc
Daniel Vinagre Luengo <60425460+Da-viel@users.noreply.github.com>
2022-12-11 17:21:32 +0100

    Update parametrizadoDSL2.groovy

--- parametrizadoDSL2.groovy
+++ parametrizadoDSL2.groovy
@@ -7,7 +7,7 @@ job('ejemplo3-job-DSL') {
       		}
     	} 
   	parameters {
-   		stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
+   		stringParam('nombre', defaultValue = 'Daniel', description = 'Parametro de cadena para el Job Booleano')
       		choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
       		booleanParam('agente', false)
     	}
